8429e849809457809fd1b352dc2475679cedd4cb
kacper.czerwinski <kaczet@gmail.com>
2020-06-12 12:00:16 +0200

    animated text

--- src/app/modules/game/containers/play/play.component.ts
+++ src/app/modules/game/containers/play/play.component.ts
@@ -2,17 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
 import { GameService } from 'src/app/modules/game/services/game.service';
 import { CurrentGame } from 'src/app/modules/game/interfaces/current-game.interface';
+import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
     selector: 'kr-play',
     templateUrl: './play.component.html',
-    styleUrls: ['./play.component.scss']
+    styleUrls: ['./play.component.scss'],
+    animations: [
+      trigger('slideInOut', [
+        transition(':enter', [
+          style({transform: 'translateY(-100%)'}),
+          animate('200ms ease-in', style({transform: 'translateY(0%)'}))
+        ]),
+        transition(':leave', [
+          animate('200ms ease-in', style({transform: 'translateY(-100%)'}))
+        ])
+      ])
+    ]
 })
 export class PlayComponent implements OnInit {
     play: boolean;
     progressbarValue = 0;
     curSec = 0;
     currentGame: CurrentGame;
+    visible: boolean;
     constructor(private gameService: GameService) {}
 
     ngOnInit(): void {
@@ -27,17 +40,16 @@ export class PlayComponent implements OnInit {
 
     startTimer() {
         this.loadCurrentGame();
-        this.play = true;
+        this.play = false;
         const timer$ = interval(1000);
-
         const sub = timer$.subscribe(sec => {
             this.progressbarValue = (sec * 100) / 5;
             this.curSec = sec;
 
             if (this.curSec === 6) {
                 sub.unsubscribe();
-                this.play = false;
                 this.progressbarValue = 0;
+                this.loadCurrentGame();
             }
         });
     }
